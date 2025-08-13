8d14e6525ffa8052738c30e10592930e22d5db54
Scott Moss <willscottmoss@gmail.com>
2025-08-13 14:06:22 -0500

    solution

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from 'express'
+import authRoutes from './routes/authRoutes.ts'
+import userRoutes from './routes/userRoutes.ts'
+import habitRoutes from './routes/habitRoutes.ts'
 
 const app = express()
 
 app.get('/health', (req, res) => {
   res.send('<button>click</click>')
 })
+
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/habits', habitRoutes)
 
 export { app }
 
