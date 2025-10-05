159de52a870fee7c4fdacf0e626878f4b78b6638
Mona Raj <agrmonaraj@gmail.com>
2025-10-05 20:19:12 +0530

    Add authentication, user, and habit routes

---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+
+const router = Router()
+
+router.post('/register', (req, res) => {
+  res.status(201).json({
+    message: 'user signed up',
+  })
+})
+
+router.post('/login', (req, res) => {
+  res.status(201).json({
+    message: 'user logged in',
+  })
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from 'express'
+import authRoutes from './routes/authRoutes.ts'
+import userRoutes from './routes/userRoutes.ts'
+import habitRoutes from './routes/habitRoutes.ts'
 
 const app = express()
 
 app.get('/health', (req, res) => {
   res.json({ message: 'hello' }).status(200)
 })
+
+// mounting
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/habits', habitRoutes)
 
 export { app }
 
