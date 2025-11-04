1b6450237e85f4002b8620dce2ea715b6a838a34
Mona Raj <agrmonaraj@gmail.com>
2025-11-04 15:10:36 +0530

    Implement validation middleware for request body, params, and query

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,22 @@ import express from 'express'
 import authRoutes from './routes/authRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
+import cors from 'cors' // cross origin resource sharing
+import helmet from 'helmet' // security
+import morgan from 'morgan' // request logs
+import { isTest } from '../env.ts'
 
 const app = express()
+
+app.use(helmet()) // Higher order function
+app.use(cors()) // global middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTest(),
+  })
+)
 
 app.get('/health', (req, res) => {
   res.json({ message: 'hello' }).status(200)
